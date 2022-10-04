611999f5ba1298799396de0311cdfbac3ad4b9f1
Oliver C <ojc011@yahoo.com>
2022-10-04 17:59:09 -0500

    Scaffolded out base layer of project, created Home page route, added home-slider component, and uploaded images for slider.

---
+++ src/components/home-slider/home-slider.component.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+import alexis from '../../assets/Alexis-2.jpg';
+
+const HomeSlider = () => {
+  return (
+    <Carousel>
+      <div>
+        <img src={alexis} alt="alexis" />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img src="assets/2.jpeg" />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img src="assets/3.jpeg" />
+        <p className="legend">Legend 3</p>
+      </div>
+    </Carousel>
+  );
+};
+
+export default HomeSlider;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,7 @@
+import HomeSlider from '../../components/home-slider/home-slider.component';
+
+const Home = () => {
+  return <HomeSlider />;
+};
+
+export default Home;
