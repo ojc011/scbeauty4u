cea7b6780dc1efed1a97753ed146ffa06c72c552
Oliver C <ojc011@yahoo.com>
2022-10-05 16:25:35 -0500

    Successfuly created carousel slider for client pictures using require context to import all of the images from the assets folder

--- src/components/home-slider/home-slider.component.jsx
+++ src/components/home-slider/home-slider.component.jsx
@@ -1,26 +1,182 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-import alexis from '../../assets/Alexis-2.jpg';
+import { SliderContainer } from './home-slider.styles';
 
 const HomeSlider = () => {
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item) => {
+      images[item.replace('./', '')] = r(item);
+      return images;
+    });
+    return images;
+  }
+
+  const images = importAll(require.context('../../assets', false, /.jpg/));
   return (
-    <Carousel>
-      <div>
-        <img src={alexis} alt="alexis" />
-        <p className="legend">Legend 1</p>
-      </div>
-      <div>
-        <img src="assets/2.jpeg" />
-        <p className="legend">Legend 2</p>
-      </div>
-      <div>
-        <img src="assets/3.jpeg" />
-        <p className="legend">Legend 3</p>
-      </div>
-    </Carousel>
+    <SliderContainer>
+      <Carousel>
+        <div>
+          <img src={images['Alexis-2.jpg']} alt="Alexis" />
+        </div>
+        <div>
+          <img src={images['Alexis-3.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Alexis-4.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Alexis-9.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Alexis-10.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Alison-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Jordyn_and_Bob-281-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Jordyn_and_Bob-438-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['bri-wedding-2-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['bri-wedding-3-719x479.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Debbie-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Debbie2-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Glenna-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Glenna2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Harleigh-2-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Harleigh-3-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Harleigh-4-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Harleigh-5-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Harleigh-6-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Harleigh-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Kyle-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Kyle2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Laura--480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Laura-2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Laura-3-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Laura-4-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-3-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-4-1-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-4-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-5-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-6-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-9-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-10-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-and-Sean-2-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Liz-and-Sean-3-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Lizzie-3-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Lizzie-4-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Lizzie-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['MaKinzi-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['MaKinzi2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Mariana-2-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Mariana-750x500.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Mary-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Mary2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Mary3-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['MaryBM-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['MaryBM2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Natalia-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['Timbre-2-480x320.jpg']} alt="" />
+        </div>
+        <div>
+          <img src={images['timbre-480x320.jpg']} alt="" />
+        </div>
+      </Carousel>
+    </SliderContainer>
   );
 };
 
---
+++ src/components/home-slider/home-slider.styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const SliderContainer = styled.div`
+  width: 50%;
+  margin: auto;
+  margin-top: 5%;
+  display: block;
+`;
+
+export const Legend = styled.p`
+  position: absolute;
+  top: 2%;
+  left: 5%;
+  color: #ffff;
+`;
